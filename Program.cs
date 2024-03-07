7e18d7fb8404e2b23298e8a7c221a77c90812c01
Jon Shaw <jonshaw@Jons-MacBook-Pro.local>
2024-03-06 23:18:56 -0800

    initial fruitstand commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Dynamic;
+using Microsoft.VisualBasic;
+
+namespace Name
+{
+    
+}
+class FruitStand() {
+    
+}
+
+public class Banana() : Fruit{
+    public string name = "banana";
+}
+
+public interface Fruit {
+    public string name {get; set;}
+};
+
+FruitStand myStand = new FruitStand()
